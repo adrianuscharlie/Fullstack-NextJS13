584afc26064701100068b86f60d480f3b2a6ef44
adrianuscharlie <adrianus.charlie02@gmail.com>
2023-07-29 23:31:53 +0700

    fix title bug

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
-"use client";
 import "@styles/globals.css";
-import { Inter } from "next/font/google";
 import Provider from "@components/Provider";
 import Nav from "@components/Nav";
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata = {
   title: "Sambatdotcom",
@@ -13,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <Provider>
           <div className="main">
             <div className="gradient" />
